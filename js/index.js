24bbc832642c8c63fcca51bab4fb59beec6527b8
Lucas <lucasmganga11@gmail.com>
2020-04-15 15:02:55 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
 var name = "Susan"
 var height = "70"
+var name = "Susan"
+var height = "74"
+
+if (name === "Susan") {
+  console.log("The name is correct")
+} else {
+  console.log("Expected the name to be Susan")
+   }
+// Don't worry about this
+module.exports = { name, height
+}
 
 
 // Don't worry about this
